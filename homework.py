8a1d26032b4f9094c64e26a66ab2cc64639a508c
abood <intelligentg47@gmail.com>
2024-09-14 00:54:51 +0300

    initial work

---
+++ homework.py
@@ -0,0 +1,16 @@
+# DFS algorithm
+def dfs(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+
+    visited.add(start)
+    print(start, end=" ")
+
+    for neighbor in graph[start]:
+        if neighbor not in visited:
+            dfs(graph, neighbor, visited)
+                            
+if __name__ == '__main__':
+    graph = {0: [1, 2], 1: [2], 2: [3], 3: [1, 2]}
+    print("Following is Depth First Traversal: ")
+    dfs(graph, 0)
